ef82979c2efce69196d59ff61982c596f6eea82c
francesco <info.francescobianco@gmail.com>
2025-09-26 14:57:25 +0200

    Updates

--- scripts/finali-1/download.sh
+++ scripts/finali-1/download.sh
@@ -4,6 +4,7 @@ cd "pendrive/Finali 1" || exit
 
 yt-dlp \
   --yes-playlist \
+  --merge-output-format mp4 \
   -o "%(playlist_index)s - %(title)s.%(ext)s" \
-  -f "bestvideo+bestaudio/best" \
+  -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]" \
   "https://www.youtube.com/playlist?list=PL8iaAgXaStUepsJ3gRHUme43AFvNZNXDr"
